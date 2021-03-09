b96c8a2a7d247cd4a0b443cf9e00c329b65fee22
Uutka <jmwtpgad56@gmail.com>
2021-03-09 16:34:11 +0300

    Созданы запросы findOne и findAll для модуля users

---
+++ src/modules/users/dto/response/user.dto.ts
@@ -0,0 +1,7 @@
+export class UserDto {
+  id: number;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  password: string;
+}
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './models/user.entity';
+import { UserDto } from './dto/response/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<UserDto[]> {
+    return this.usersService.findAll();
+  }
+}
